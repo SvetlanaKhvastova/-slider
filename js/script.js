b84aeda93e9c9c270981ded4b2495b52f2661266
Svetlana Khvastova <SvetlanaKhvastova@gmail.com>
2020-08-19 20:47:36 +0300

    slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    cssEase: "linear",
+    pauseOnHover: true,
+  })
+})
